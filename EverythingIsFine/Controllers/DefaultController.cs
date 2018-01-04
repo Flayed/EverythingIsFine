f3034a93cc2e4a33509d94943a748206a3995210
Robert Darchuk <Robert.Darchuk@digikey.com>
2018-01-03 18:21:10 -0600

    Init

---
+++ EverythingIsFine/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EverythingIsFine.Controllers
+{
+    [Route("")]
+    public class DefaultController : Controller
+    {        
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
